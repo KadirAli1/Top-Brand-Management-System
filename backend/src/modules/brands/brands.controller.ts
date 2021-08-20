4708f68b3f0a5e0d6561fcb3afc9e0254f22d0db
Kadir Ali <kadirali552@gmail.com>
2021-08-20 10:17:57 +0200

    Updated brand controller with two endpoints

--- backend/src/modules/brands/brands.controller.ts
+++ backend/src/modules/brands/brands.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { BrandsService } from './brands.service';
 
 @Controller('brands')
-export class BrandsController {}
+export class BrandsController {
+  constructor(private brndsService: BrandsService) {}
+
+  @Get()
+  findAll() {
+    return this.brndsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.brndsService.findOne(id);
+  }
+}
