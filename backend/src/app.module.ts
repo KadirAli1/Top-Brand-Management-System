be2d62fbd0105fa6544fcf2815eb73eae03febdd
Kadir Ali <kadirali552@gmail.com>
2021-08-20 10:33:34 +0200

    connected with DB using typeORM and imports typeormModule in Brand Module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BrandsModule } from './modules/brands/brands.module';
+import { Brand } from './modules/brands/entity/brand.entity';
 
 @Module({
-  imports: [BrandsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'top_brands_management_system',
+      entities: [Brand],
+      synchronize: true,
+    }),
+    BrandsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/modules/brands/brands.module.ts
+++ backend/src/modules/brands/brands.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BrandsController } from './brands.controller';
 import { BrandsService } from './brands.service';
+import { Brand } from './entity/brand.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Brand])],
   providers: [BrandsService],
   controllers: [BrandsController],
 })
