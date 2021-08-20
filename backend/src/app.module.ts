3a6f3d5066fcb3ee5e40a381520baf9112cce6c0
Kadir Ali <kadirali552@gmail.com>
2021-08-20 10:00:17 +0200

    Created Brand module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BrandsModule } from './brands/brands.module';
+import { BrandsService } from './brands/brands.service';
 
 @Module({
-  imports: [],
+  imports: [BrandsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, BrandsService],
 })
 export class AppModule {}
