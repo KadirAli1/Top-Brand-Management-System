a1974da042dd7f94752c8458973509e3b5359e98
Kadir Ali <kadirali552@gmail.com>
2021-08-20 10:26:08 +0200

    created create update and delete brand in service and controller

--- backend/src/modules/brands/brands.controller.ts
+++ backend/src/modules/brands/brands.controller.ts
@@ -1,17 +1,43 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { BrandsService } from './brands.service';
+import { CreateUpdateBrandDto } from './dto/creaate-update-brand.dto';
 
 @Controller('brands')
 export class BrandsController {
-  constructor(private brndsService: BrandsService) {}
+  constructor(private brandsService: BrandsService) {}
 
   @Get()
   findAll() {
-    return this.brndsService.findAll();
+    return this.brandsService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: number) {
-    return this.brndsService.findOne(id);
+    return this.brandsService.findOne(id);
+  }
+  @Post()
+  create(@Body() createBrandDto: CreateUpdateBrandDto) {
+    return this.brandsService.create(createBrandDto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() updateBrandDto: CreateUpdateBrandDto,
+  ) {
+    return this.brandsService.update(id, updateBrandDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.brandsService.delete(id);
   }
 }
--- backend/src/modules/brands/brands.service.ts
+++ backend/src/modules/brands/brands.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateUpdateBrandDto } from './dto/creaate-update-brand.dto';
 import { Brand } from './entity/brand.entity';
 
 @Injectable()
@@ -15,5 +16,16 @@ export class BrandsService {
 
   findOne(id: number) {
     return this.brandsRepository.findOne(id);
+  }
+  create(createBrandDto: CreateUpdateBrandDto) {
+    return this.brandsRepository.save(createBrandDto);
+  }
+
+  update(id: number, updateBrandDto: CreateUpdateBrandDto) {
+    return this.brandsRepository.update(id, updateBrandDto);
+  }
+
+  delete(id: number) {
+    return this.brandsRepository.delete(id);
   }
 }
